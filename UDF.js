f4c59100d31bd0fcdd8a686efae5a5202e34a81b
SUDEEP ADHYA <adhya_sudeep@network.lilly.com>
2020-10-29 10:52:56 +0530

    Update UDF.js

--- UDF.js
+++ UDF.js
@@ -2,9 +2,9 @@ function transform(line) {
 var values = line.split(',');
 
 var obj = new Object();
-obj.location = values[0];
+obj.id = values[0];
 obj.name = values[1];
-obj.age = values[2];
+obj.department = values[2];
 var jsonString = JSON.stringify(obj);
 
 return jsonString;
